d2f4eb97edcff360c45b7239d8597c5cb478c7f5
Manos Miras <manosmiras@outlook.com>
2024-10-12 23:47:58 +0300

    button component

---
+++ components/Button.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+   defineProps({
+    variant: {
+      type: String as 'primary' | 'secondary',
+      default: 'primary',
+    },
+    to: {
+      type: String
+    }
+  })
+</script>
+
+<template>
+  <RouterLink :to="to" :class="variant" class="button">
+      <slot></slot>
+  </RouterLink>
+</template>
+
+<style scoped>
+  .button {
+    @apply hover:cursor-pointer rounded-full font-semibold px-6 py-2
+  }
+
+  .primary {
+    @apply bg-primary text-secondary;
+  }
+
+  .secondary {
+    @apply bg-secondary;
+  }
+</style>
