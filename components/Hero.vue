741c01bc9806fef6d3971b79e1155d11a95268f0
Manos Miras <manosmiras@outlook.com>
2025-08-14 16:35:46 +0300

    Move videos to public and add a reel

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="static">
-    <video autoplay muted loop class="object-cover w-screen h-screen blur-xs brightness-90 blur-sm">
-      <source src="@/assets/videos/Moirai.mp4" type="video/mp4">
+    <video autoplay muted loop class="object-cover w-screen h-screen blur-sm">
+      <source src="/videos/reel.mp4" type="video/mp4">
     </video>
     <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-default text-center">
       <div class="text-7xl font-bold">Manos Miras</div>
