e39412d1a240ffa19c267d8bac94f31d0af27fa1
Manos Miras <manosmiras@outlook.com>
2025-08-28 14:23:24 +0300

    title instead of div

--- components/Hero.vue
+++ components/Hero.vue
@@ -4,7 +4,7 @@
       <source src="/videos/reel.mp4" type="video/mp4">
     </video>
     <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-default text-center">
-      <div class="text-7xl font-bold">Manos Miras</div>
+      <title class="text-7xl font-bold">Manos Miras</title>
       <div class="text-3xl font-semibold">Software Engineer & Game Developer</div>
       <div class="flex justify-self-center gap-6 p-4">
         <Button to="#portfolio">Portfolio</Button>
